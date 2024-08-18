735393705457b719666765b051056408fcb76354
hyultis <hyultis@gmail.com>
2024-08-18 15:27:01 +0200

    Updated default behavior : default minimum duration is now 1ns instead of 16ms (60fps) Added thread_launch_delayabe(), that put a additionnal run of the function at the end of the running one (cannot stack)

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,14 @@ use thread_priority::ThreadBuilder;
 pub type ThreadPriority = thread_priority::ThreadPriority;
 pub type ThreadPriorityValue = thread_priority::ThreadPriorityValue;
 
+#[derive(Copy, Clone, Debug)]
+enum Loop
+{
+	NO,
+	ONE,
+	YES
+}
+
 pub struct SingletonThread
 {
 	_thread: Option<JoinHandle<()>>,
@@ -18,7 +26,7 @@ pub struct SingletonThread
 	_threadFunc: Arc<RwLock<Box<dyn FnMut() + Send + Sync + 'static>>>,
 	_filterPrefunc: Box<dyn FnMut() -> bool + Send + Sync + 'static>,
 	_durationmin: Duration,
-	_loop: Arc<RwLock<bool>>,
+	_loop: Arc<RwLock<Loop>>,
 }
 
 impl SingletonThread
@@ -26,18 +34,10 @@ impl SingletonThread
 	/// create a new singletonThread
 	pub fn new(mainFn: impl Fn() + Send + Sync + 'static) -> Self
 	{
-		return SingletonThread {
-			_thread: None,
-			_threadName: None,
-			_threadPriority: ThreadPriority::Crossplatform(ThreadPriorityValue::default()),
-			_threadFunc: Arc::new(RwLock::new(Box::new(mainFn))),
-			_filterPrefunc: Box::new(||{true}),
-			_durationmin: Duration::from_millis(17), // default 60 fps
-			_loop: Arc::new(RwLock::new(false)),
-		};
+		return Self::newFiltered(mainFn,||{true});
 	}
 	
-	/// create a new singletonThread with a filter
+	/// create a new singletonThread with a filter.
 	/// filter is run each time, after testing if thread is running or not
 	/// filter must return true if the thread can be launched, or false if not
 	pub fn newFiltered(mainFn: impl Fn() + Send + Sync + 'static, filterFn: impl Fn() -> bool + Send + Sync + 'static) -> Self
@@ -48,14 +48,67 @@ impl SingletonThread
 			_threadPriority: ThreadPriority::Crossplatform(ThreadPriorityValue::default()),
 			_threadFunc: Arc::new(RwLock::new(Box::new(mainFn))),
 			_filterPrefunc: Box::new(filterFn),
-			_durationmin: Duration::from_millis(17), // default 60 fps
-			_loop: Arc::new(RwLock::new(false)),
+			_durationmin: Duration::from_nanos(1),
+			_loop: Arc::new(RwLock::new(Loop::NO)),
 		};
 	}
 	
 	/// try to launch a new run, check if not running AND the filter function return true
 	/// return if the thread have been launched
 	pub fn thread_launch(&mut self) -> bool
+	{
+		return self.internal_thread_launch(false);
+	}
+	
+	/// try to launch a new run, check if not running AND the filter function return true
+	/// if it cannot be launched immediately, mark the class to run another time after the current one. can only mark one time.
+	pub fn thread_launch_delayabe(&mut self)
+	{
+		self.internal_thread_launch(true);
+	}
+	
+	/// minimum duration before another run can occur, default 1ns
+	pub fn setDuration(&mut self, duration: Duration)
+	{
+		self._durationmin = duration;
+	}
+	
+	/// minimum duration before another run can occur
+	pub fn setDuration_FPS(&mut self,fps: u8)
+	{
+		let durationmillis = 1000/fps as u64;
+		self._durationmin = Duration::from_millis(durationmillis);
+	}
+	
+	/// define if the thread auto relaunch himself (technically it just use the same thread)
+	/// set to false if you want to stop a looped thread that running.
+	pub fn setLoop(&mut self, canLoop: bool)
+	{
+		if(canLoop)
+		{
+			*self._loop.write() = Loop::YES;
+		}
+		else
+		{
+			*self._loop.write() = Loop::NO;
+		}
+	}
+	
+	pub fn setThreadName(&mut self, name: impl Into<String>)
+	{
+		let name = name.into();
+		self._threadName = Some(name);
+	}
+	
+	pub fn setThreadPriority(&mut self, priority: ThreadPriority)
+	{
+		self._threadPriority = priority;
+	}
+	
+	////// PRIVATE
+	
+	
+	fn internal_thread_launch(&mut self,delayabe: bool) -> bool
 	{
 		let mut canLaunch = match &self._thread {
 			None => true,
@@ -72,6 +125,15 @@ impl SingletonThread
 		
 		if (!canLaunch)
 		{
+			if(delayabe)
+			{
+				let tmp = {*self._loop.clone().read()};
+				match tmp
+				{
+					Loop::NO => {*self._loop.clone().write() = Loop::ONE}
+					_ => {}
+				}
+			}
 			return false;
 		}
 		
@@ -92,45 +154,13 @@ impl SingletonThread
 		if let Ok(spawned) = thread.spawn(spawnfunc)
 		{
 			self._thread = Some(spawned);
+			return true;
 		}
 		
-		return true;
+		return false;
 	}
 	
-	/// minimum duration before another run can occur, default 166ms (60fps)
-	pub fn setDuration(&mut self, duration: Duration)
-	{
-		self._durationmin = duration;
-	}
-	
-	/// minimum duration before another run can occur, default 166ms (60fps)
-	pub fn setDuration_FPS(&mut self,fps: u8)
-	{
-		let durationmillis = 1000/fps as u64;
-		self._durationmin = Duration::from_millis(durationmillis);
-	}
-	
-	/// define if the thread auto relaunch himself (technically it just use the same thread)
-	/// set to false if you want to stop a looped thread that running.
-	pub fn setLoop(&mut self, canLoop: bool)
-	{
-		*self._loop.write() = canLoop;
-	}
-	
-	pub fn setThreadName(&mut self, name: impl Into<String>)
-	{
-		let name = name.into();
-		self._threadName = Some(name);
-	}
-	
-	pub fn setThreadPriority(&mut self, priority: ThreadPriority)
-	{
-		self._threadPriority = priority;
-	}
-	
-	////// PRIVATE
-	
-	fn ThreadFunc(arced: Arc<RwLock<Box<dyn FnMut() + Send + Sync>>>, arcloop: Arc<RwLock<bool>>, durationtowait: Duration)
+	fn ThreadFunc(arced: Arc<RwLock<Box<dyn FnMut() + Send + Sync>>>, arcloop: Arc<RwLock<Loop>>, durationtowait: Duration)
 	{
 		loop
 		{
@@ -146,9 +176,14 @@ impl SingletonThread
 				sleep(durationtowait - timeelapsed);
 			}
 			
-			if !*arcloop.read()
+			let tmp = {*arcloop.read()};
+			match tmp
 			{
-				break;
+				Loop::NO => {return;}
+				Loop::ONE => {
+					*arcloop.write()=Loop::NO;
+				}
+				Loop::YES => {}
 			}
 		}
 	}
